888eccaa7757c51fd41048cac28c1b49cf36a6be
rsazidur <rsazidur936@gmail.com>
2025-07-11 02:16:21 +0600

    Change the Background

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { use } from 'react'
 import { assets } from '../assets/assets'
-import { Calendar, Clock } from "lucide-react";
+import { ArrowRight, CalendarIcon, ClockIcon } from "lucide-react";
+import { useNavigate } from 'react-router-dom'
 
 
 
 const HeroSection = () => {
+
+    const navigate = useNavigate();
+
   return (
     <div className='flex flex-col items-start justify-center gap-4 
-    px-6 md:px-16 lg:px-36 bg-[url("/background.png")] 
+    px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.jpg")] 
     bg-cover bg-center h-screen '>
-        <img src={assets.marelLogo} alt="" className='max-h-11 lg:h-11 mt-20'/>
+        <img src={assets.marvelLogo} alt="" className='max-h-11 lg:h-11 mt-20'/>
 
         <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold
         max-w-110'>Guardians <br /> of the Galaxy </h1>
@@ -17,12 +21,21 @@ const HeroSection = () => {
         <div className='flex items-center gap-4 text-gray-300'>
             <span>Action | Advebture | Sci-Fi</span>
             <div className='flex items-center gap-1'>
-                <Calendar className='w-4.5 h-4.5' /> 2018
+                <CalendarIcon className='w-4.5 h-4.5' /> 2018
             </div>
             <div className='flex items-center gap-1'>
-                <Clock className='w-4.5 h-4.5' /> 2h 16 min
+                <ClockIcon className='w-4.5 h-4.5' /> 2h 16 min
             </div>
-        </div>    
+        </div>
+        <p className='max-w-md text-gray-300'>Peter Quill (Chris Pratt) steals a mysterious orb, making him 
+          the target of a powerful villain, Ronan. To survive, he teams up with a group of misfits — Rocket 
+          Raccoon, Groot, Gamora, and Drax. When they learn the orb holds a cosmic threat, they must work 
+          together to save the universe.</p>
+          <button onClick={()=> navigate('/movies')} className='flex items-center gap-1 px-6 py-3 text-sm bg-primary
+          hover:bg-primary-dull transition-rounded-full font-medium cursor-pointer'>
+            Explore Movies
+            <ArrowRight className='w-5 h-5'/>
+          </button>    
     </div>
 
   )
