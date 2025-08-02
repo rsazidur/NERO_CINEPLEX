82542c8bf6b88955f83fac97f05f436a37f5e168
rsazidur <rsazidur936@gmail.com>
2025-08-03 04:51:36 +0600

    Made a change in Trailer section

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { StarIcon } from 'lucide-react'
+import { timeFormat } from '../lib/timeFormat';
 
 const MovieCard = ({movie}) => {
 
@@ -18,7 +19,7 @@ const MovieCard = ({movie}) => {
 
         <p className="text-sm text-gray-400 mt-2">
             {new Date(movie.release_date).getFullYear()} · {" "}
-            {movie.genres.slice(0, 2).map(genre => genre.name).join(" | ")} · {movie.runtime}
+            {movie.genres.slice(0, 2).map(genre => genre.name).join(" | ")} · {timeFormat(movie.runtime)}
         </p>
 
         <div className='flex items-center justify-between mt-4 pb-3'>
--- frontend/src/lib/timeFormat.js
+++ frontend/src/lib/timeFormat.js
@@ -2,7 +2,7 @@
 const timeFormat = (minutes) => {
     const hours = Math.floor(minutes / 60);
     const minutesRemainder = minutes % 60;
-    return `${hours}h ${minutesRemainder}m`;
-};
+    return `${hours}h ${minutesRemainder}m`
+}
 
-export default timeFormat;
+export { timeFormat };
