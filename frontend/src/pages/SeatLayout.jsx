f190f25e6c96e09266688261a25963e0fb62464c
rsazidur <rsazidur936@gmail.com>
2025-08-13 03:07:36 +0600

    Available Times

--- frontend/src/pages/SeatLayout.jsx
+++ frontend/src/pages/SeatLayout.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import Loading from '../components/Loading'
+import { ClockIcon } from 'lucide-react'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import { useState } from 'react'
 
 const SeatLayout = () => {
 
@@ -28,14 +31,25 @@ const SeatLayout = () => {
   return show ? (
     <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30
     md:pt-50'>
-      {/* Available Seats */}
-      <div className='w-60 bg-primary/10 border border-primary/20 rounded-lg py-10
-      h-max md:sticky md:top-30'></div>
-
+      {/* Available Timings */}
+      <div className='w-60 bg-primary/10 border border-primary/20 rounded-b-lg py-10
+      h-max md:sticky md:top-30'>
+        <p className='text-lg font-semibold px-6'>Available Timings</p>
+        <div className='mt-5 space-y-1'>
+          {show.dateTime[date].map(item => (
+            <div className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md
+            cursor-pointer transition ${selectedTime?.time === item.time ?
+              "bg-primary text-white" : "hover:bg-primary/20"}`}>
+              <ClockIcon className='w-4 h-4' />
+              <p className='text-sm'>{item.time}</p>
+            </div>
+          ))}
+        </div>
+      </div>
       {/* Seats Layout */}
       <div></div>
-
     </div>
+  
   ) : (
     <Loading />
   )
