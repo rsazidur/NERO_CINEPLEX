6b550edd3ec7c4a9685ba7226611466b7a3fa12b
rsazidur <rsazidur936@gmail.com>
2025-08-23 23:01:22 +0600

    BookingList In Admin

--- frontend/src/pages/admin/ListBookings.jsx
+++ frontend/src/pages/admin/ListBookings.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import dummyBookingData from '../../dummyData/dummyBookingData'
+import Loading from '../../components/Loading'
+import Title from '../../components/admin/Title'
 
 const ListBookings = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [bookings, setBookings] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getAllBookings = async () => {
+    setBookings(dummyBookingData)
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getAllBookings();
+  }, []);
+
+  return !isLoading ? (
+    <>
+      <Title text1="List" text2="Bookings" />
+      <div className='max-w-4xl mt-6 overflow-x-auto'>
+        <table className='w-full border-collapse rounded-md overflow-hidden
+        text-nowrap'>
+          <thead>
+            <tr className='bg-primary/20 text-left text-white'>
+              <th className='p-2 font-medium pl-5'>User Name</th>
+              <th className='p-2 font-medium'>Movie Name</th>
+              <th className='p-2 font-medium'>Show Time</th>
+              <th className='p-2 font-medium'>Seats</th>
+              <th className='p-2 font-medium'>Amounts</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
+  ) : <Loading/>
 }
 
 export default ListBookings
